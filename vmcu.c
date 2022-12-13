99f09c5ec9b3ccca478f44ea49c0ed62e128b025
Mikko Salomäki <ms@datarespons.se>
2022-12-13 20:17:32 +0100

    Bump version to 1.1

--- vmcu.c
+++ vmcu.c
@@ -1335,4 +1335,4 @@ module_i2c_driver(vmcu_driver);
 MODULE_AUTHOR("Mikko Salomäki <ms@datarespons.se>");
 MODULE_DESCRIPTION("Vehicle MCU driver");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.0");
+MODULE_VERSION("1.1");
